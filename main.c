d9d6e4625b31d04a2fe453eaa739cd7bd6309e8d
adl <adluong11@gmail.com>
2025-05-15 13:55:11 +0900

    modify main

--- main.c
+++ main.c
@@ -4,22 +4,21 @@
 #include <stdlib.h>
 
 #include "api.h"
-#include "rng.h"
-#include "sign.h"
-#include "cpucycles.h"
+#include "common/rng.h"
+#include "common/cpucycles.h"
 
 #define MLEN 59
 #define TEST_LOOP 1000
 
 unsigned long long cyclegap = 0;
 
-static void TEST_SIGN(void)
+static void TEST_SIGN_AIMER128F(void)
 {
-    unsigned char m[MLEN + CRYPTO_BYTES];
-    unsigned char m2[MLEN + CRYPTO_BYTES];
-    unsigned char sm[MLEN + CRYPTO_BYTES];
-    unsigned char pk[CRYPTO_PUBLICKEYBYTES];
-    unsigned char sk[CRYPTO_SECRETKEYBYTES];
+    unsigned char m[MLEN + kucp_aimer128f_BYTES];
+    unsigned char m2[MLEN + kucp_aimer128f_BYTES];
+    unsigned char sm[MLEN + kucp_aimer128f_BYTES];
+    unsigned char pk[kucp_aimer128f_PUBLICKEYBYTES];
+    unsigned char sk[kucp_aimer128f_SECRETKEYBYTES];
 
     int ret;
     unsigned long long mlen, smlen;
@@ -30,11 +29,11 @@ static void TEST_SIGN(void)
     {
         randombytes(m, MLEN);
 
-        crypto_sign_keypair(pk, sk);
+        kucp_aimer128f_aarch64_crypto_sign_keypair(pk, sk);
 
-        crypto_sign(sm, &smlen, m, MLEN, sk);
+        kucp_aimer128f_aarch64_crypto_sign(sm, &smlen, m, MLEN, sk);
 
-        ret = crypto_sign_open(m2, &mlen, sm, smlen, pk);
+        ret = kucp_aimer128f_aarch64_crypto_sign_open(m2, &mlen, sm, smlen, pk);
 
         if(ret)
         {
@@ -46,14 +45,178 @@ static void TEST_SIGN(void)
     printf("Verification success\n");    
 }
 
-static void TEST_SIGN_CLOCK(void)
-{   
-    
-    unsigned char m[MLEN + CRYPTO_BYTES];
-    unsigned char m2[MLEN + CRYPTO_BYTES];
-    unsigned char sm[MLEN + CRYPTO_BYTES];
-    unsigned char pk[CRYPTO_PUBLICKEYBYTES];
-    unsigned char sk[CRYPTO_SECRETKEYBYTES];
+static void TEST_SIGN_AIMER128S(void)
+{
+    unsigned char m[MLEN + kucp_aimer128s_BYTES];
+    unsigned char m2[MLEN + kucp_aimer128s_BYTES];
+    unsigned char sm[MLEN + kucp_aimer128s_BYTES];
+    unsigned char pk[kucp_aimer128s_PUBLICKEYBYTES];
+    unsigned char sk[kucp_aimer128s_SECRETKEYBYTES];
+
+    int ret;
+    unsigned long long mlen, smlen;
+
+	printf("============= KEYGEN SIGN VERIFY TEST ============\n");
+
+    for(int i = 0; i < TEST_LOOP; i++)
+    {
+        randombytes(m, MLEN);
+
+        kucp_aimer128s_aarch64_crypto_sign_keypair(pk, sk);
+
+        kucp_aimer128s_aarch64_crypto_sign(sm, &smlen, m, MLEN, sk);
+
+        ret = kucp_aimer128s_aarch64_crypto_sign_open(m2, &mlen, sm, smlen, pk);
+
+        if(ret)
+        {
+            printf("Verification failed\n");
+            return;
+        }
+    }
+
+    printf("Verification success\n");    
+}
+
+static void TEST_SIGN_AIMER192F(void)
+{
+    unsigned char m[MLEN + kucp_aimer192f_BYTES];
+    unsigned char m2[MLEN + kucp_aimer192f_BYTES];
+    unsigned char sm[MLEN + kucp_aimer192f_BYTES];
+    unsigned char pk[kucp_aimer192f_PUBLICKEYBYTES];
+    unsigned char sk[kucp_aimer192f_SECRETKEYBYTES];
+
+    int ret;
+    unsigned long long mlen, smlen;
+
+	printf("============= KEYGEN SIGN VERIFY TEST ============\n");
+
+    for(int i = 0; i < TEST_LOOP; i++)
+    {
+        randombytes(m, MLEN);
+
+        kucp_aimer192f_aarch64_crypto_sign_keypair(pk, sk);
+
+        kucp_aimer192f_aarch64_crypto_sign(sm, &smlen, m, MLEN, sk);
+
+        ret = kucp_aimer192f_aarch64_crypto_sign_open(m2, &mlen, sm, smlen, pk);
+
+        if(ret)
+        {
+            printf("Verification failed\n");
+            return;
+        }
+    }
+
+    printf("Verification success\n");    
+}
+
+static void TEST_SIGN_AIMER192S(void)
+{
+    unsigned char m[MLEN + kucp_aimer192s_BYTES];
+    unsigned char m2[MLEN + kucp_aimer192s_BYTES];
+    unsigned char sm[MLEN + kucp_aimer192s_BYTES];
+    unsigned char pk[kucp_aimer192s_PUBLICKEYBYTES];
+    unsigned char sk[kucp_aimer192s_SECRETKEYBYTES];
+
+    int ret;
+    unsigned long long mlen, smlen;
+
+	printf("============= KEYGEN SIGN VERIFY TEST ============\n");
+
+    for(int i = 0; i < TEST_LOOP; i++)
+    {
+        randombytes(m, MLEN);
+
+        kucp_aimer192s_aarch64_crypto_sign_keypair(pk, sk);
+
+        kucp_aimer192s_aarch64_crypto_sign(sm, &smlen, m, MLEN, sk);
+
+        ret = kucp_aimer192s_aarch64_crypto_sign_open(m2, &mlen, sm, smlen, pk);
+
+        if(ret)
+        {
+            printf("Verification failed\n");
+            return;
+        }
+    }
+
+    printf("Verification success\n");    
+}
+
+static void TEST_SIGN_AIMER256F(void)
+{
+    unsigned char m[MLEN + kucp_aimer256f_BYTES];
+    unsigned char m2[MLEN + kucp_aimer256f_BYTES];
+    unsigned char sm[MLEN + kucp_aimer256f_BYTES];
+    unsigned char pk[kucp_aimer256f_PUBLICKEYBYTES];
+    unsigned char sk[kucp_aimer256f_SECRETKEYBYTES];
+
+    int ret;
+    unsigned long long mlen, smlen;
+
+	printf("============= KEYGEN SIGN VERIFY TEST ============\n");
+
+    for(int i = 0; i < TEST_LOOP; i++)
+    {
+        randombytes(m, MLEN);
+
+        kucp_aimer256f_aarch64_crypto_sign_keypair(pk, sk);
+
+        kucp_aimer256f_aarch64_crypto_sign(sm, &smlen, m, MLEN, sk);
+
+        ret = kucp_aimer256f_aarch64_crypto_sign_open(m2, &mlen, sm, smlen, pk);
+
+        if(ret)
+        {
+            printf("Verification failed\n");
+            return;
+        }
+    }
+
+    printf("Verification success\n");    
+}
+
+static void TEST_SIGN_AIMER256S(void)
+{
+    unsigned char m[MLEN + kucp_aimer256s_BYTES];
+    unsigned char m2[MLEN + kucp_aimer256s_BYTES];
+    unsigned char sm[MLEN + kucp_aimer256s_BYTES];
+    unsigned char pk[kucp_aimer256s_PUBLICKEYBYTES];
+    unsigned char sk[kucp_aimer256s_SECRETKEYBYTES];
+
+    int ret;
+    unsigned long long mlen, smlen;
+
+	printf("============= KEYGEN SIGN VERIFY TEST ============\n");
+
+    for(int i = 0; i < TEST_LOOP; i++)
+    {
+        randombytes(m, MLEN);
+
+        kucp_aimer256s_aarch64_crypto_sign_keypair(pk, sk);
+
+        kucp_aimer256s_aarch64_crypto_sign(sm, &smlen, m, MLEN, sk);
+
+        ret = kucp_aimer256s_aarch64_crypto_sign_open(m2, &mlen, sm, smlen, pk);
+
+        if(ret)
+        {
+            printf("Verification failed\n");
+            return;
+        }
+    }
+
+    printf("Verification success\n");    
+}
+
+static void TEST_SIGN_CLOCK_AIMER128F(void)
+{
+    unsigned char m[MLEN + kucp_aimer128f_BYTES];
+    unsigned char m2[MLEN + kucp_aimer128f_BYTES];
+    unsigned char sm[MLEN + kucp_aimer128f_BYTES];
+    unsigned char pk[kucp_aimer128f_PUBLICKEYBYTES];
+    unsigned char sk[kucp_aimer128f_SECRETKEYBYTES];
 
     unsigned long long kcycles = 0;
     unsigned long long ecycles = 0;
@@ -67,8 +230,9 @@ static void TEST_SIGN_CLOCK(void)
     for(int i = 0; i < TEST_LOOP; i++)
     {
         randombytes(m, MLEN);
+
         cycles1 = cpucycles();
-        crypto_sign_keypair(pk, sk);
+        kucp_aimer128f_aarch64_crypto_sign_keypair(pk, sk);
 		cycles2 = cpucycles();
 		kcycles += cycles2-cycles1 - cyclegap;
     }
@@ -79,12 +243,247 @@ static void TEST_SIGN_CLOCK(void)
         randombytes(m, MLEN);
 
         cycles1 = cpucycles();
-        crypto_sign(sm, &smlen, m, MLEN, sk);
+        kucp_aimer128f_aarch64_crypto_sign(sm, &smlen, m, MLEN, sk);
 		cycles2 = cpucycles();
 		ecycles += cycles2-cycles1 - cyclegap;
 
         cycles1 = cpucycles();
-        crypto_sign_open(m2, &mlen, sm, smlen, pk);
+        kucp_aimer128f_aarch64_crypto_sign_open(m2, &mlen, sm, smlen, pk);
+		cycles2 = cpucycles();
+		dcycles += cycles2-cycles1 - cyclegap;
+
+    }
+    printf("  SIGN   runs in ................. %8lld cycles\n", ecycles/TEST_LOOP);
+    printf("  VERIFY runs in ................. %8lld cycles\n", dcycles/TEST_LOOP);
+}
+
+static void TEST_SIGN_CLOCK_AIMER128S(void)
+{
+    unsigned char m[MLEN + kucp_aimer128s_BYTES];
+    unsigned char m2[MLEN + kucp_aimer128s_BYTES];
+    unsigned char sm[MLEN + kucp_aimer128s_BYTES];
+    unsigned char pk[kucp_aimer128s_PUBLICKEYBYTES];
+    unsigned char sk[kucp_aimer128s_SECRETKEYBYTES];
+
+    unsigned long long kcycles = 0;
+    unsigned long long ecycles = 0;
+    unsigned long long dcycles = 0;
+	unsigned long long cycles1, cycles2;
+
+    unsigned long long mlen, smlen;
+
+	printf("========== KEYGEN SIGN VERIFY SPEED TEST =========\n");
+
+    for(int i = 0; i < TEST_LOOP; i++)
+    {
+        randombytes(m, MLEN);
+
+        cycles1 = cpucycles();
+        kucp_aimer128s_aarch64_crypto_sign_keypair(pk, sk);
+		cycles2 = cpucycles();
+		kcycles += cycles2-cycles1 - cyclegap;
+    }
+    printf("  KEYGEN runs in ................. %8lld cycles\n", kcycles/TEST_LOOP);
+	
+    for(int i = 0; i < TEST_LOOP; i++)
+    {
+        randombytes(m, MLEN);
+
+        cycles1 = cpucycles();
+        kucp_aimer128s_aarch64_crypto_sign(sm, &smlen, m, MLEN, sk);
+		cycles2 = cpucycles();
+		ecycles += cycles2-cycles1 - cyclegap;
+
+        cycles1 = cpucycles();
+        kucp_aimer128s_aarch64_crypto_sign_open(m2, &mlen, sm, smlen, pk);
+		cycles2 = cpucycles();
+		dcycles += cycles2-cycles1 - cyclegap;
+
+    }
+    printf("  SIGN   runs in ................. %8lld cycles\n", ecycles/TEST_LOOP);
+    printf("  VERIFY runs in ................. %8lld cycles\n", dcycles/TEST_LOOP);
+}
+
+static void TEST_SIGN_CLOCK_AIMER192F(void)
+{
+    unsigned char m[MLEN + kucp_aimer192f_BYTES];
+    unsigned char m2[MLEN + kucp_aimer192f_BYTES];
+    unsigned char sm[MLEN + kucp_aimer192f_BYTES];
+    unsigned char pk[kucp_aimer192f_PUBLICKEYBYTES];
+    unsigned char sk[kucp_aimer192f_SECRETKEYBYTES];
+
+    unsigned long long kcycles = 0;
+    unsigned long long ecycles = 0;
+    unsigned long long dcycles = 0;
+	unsigned long long cycles1, cycles2;
+
+    unsigned long long mlen, smlen;
+
+	printf("========== KEYGEN SIGN VERIFY SPEED TEST =========\n");
+
+    for(int i = 0; i < TEST_LOOP; i++)
+    {
+        randombytes(m, MLEN);
+
+        cycles1 = cpucycles();
+        kucp_aimer192f_aarch64_crypto_sign_keypair(pk, sk);
+		cycles2 = cpucycles();
+		kcycles += cycles2-cycles1 - cyclegap;
+    }
+    printf("  KEYGEN runs in ................. %8lld cycles\n", kcycles/TEST_LOOP);
+	
+    for(int i = 0; i < TEST_LOOP; i++)
+    {
+        randombytes(m, MLEN);
+
+        cycles1 = cpucycles();
+        kucp_aimer192f_aarch64_crypto_sign(sm, &smlen, m, MLEN, sk);
+		cycles2 = cpucycles();
+		ecycles += cycles2-cycles1 - cyclegap;
+
+        cycles1 = cpucycles();
+        kucp_aimer192f_aarch64_crypto_sign_open(m2, &mlen, sm, smlen, pk);
+		cycles2 = cpucycles();
+		dcycles += cycles2-cycles1 - cyclegap;
+
+    }
+    printf("  SIGN   runs in ................. %8lld cycles\n", ecycles/TEST_LOOP);
+    printf("  VERIFY runs in ................. %8lld cycles\n", dcycles/TEST_LOOP);
+}
+
+static void TEST_SIGN_CLOCK_AIMER192S(void)
+{
+    unsigned char m[MLEN + kucp_aimer192s_BYTES];
+    unsigned char m2[MLEN + kucp_aimer192s_BYTES];
+    unsigned char sm[MLEN + kucp_aimer192s_BYTES];
+    unsigned char pk[kucp_aimer192s_PUBLICKEYBYTES];
+    unsigned char sk[kucp_aimer192s_SECRETKEYBYTES];
+
+    unsigned long long kcycles = 0;
+    unsigned long long ecycles = 0;
+    unsigned long long dcycles = 0;
+	unsigned long long cycles1, cycles2;
+
+    unsigned long long mlen, smlen;
+
+	printf("========== KEYGEN SIGN VERIFY SPEED TEST =========\n");
+
+    for(int i = 0; i < TEST_LOOP; i++)
+    {
+        randombytes(m, MLEN);
+
+        cycles1 = cpucycles();
+        kucp_aimer192s_aarch64_crypto_sign_keypair(pk, sk);
+		cycles2 = cpucycles();
+		kcycles += cycles2-cycles1 - cyclegap;
+    }
+    printf("  KEYGEN runs in ................. %8lld cycles\n", kcycles/TEST_LOOP);
+	
+    for(int i = 0; i < TEST_LOOP; i++)
+    {
+        randombytes(m, MLEN);
+
+        cycles1 = cpucycles();
+        kucp_aimer192s_aarch64_crypto_sign(sm, &smlen, m, MLEN, sk);
+		cycles2 = cpucycles();
+		ecycles += cycles2-cycles1 - cyclegap;
+
+        cycles1 = cpucycles();
+        kucp_aimer192s_aarch64_crypto_sign_open(m2, &mlen, sm, smlen, pk);
+		cycles2 = cpucycles();
+		dcycles += cycles2-cycles1 - cyclegap;
+
+    }
+    printf("  SIGN   runs in ................. %8lld cycles\n", ecycles/TEST_LOOP);
+    printf("  VERIFY runs in ................. %8lld cycles\n", dcycles/TEST_LOOP);
+}
+
+static void TEST_SIGN_CLOCK_AIMER256F(void)
+{
+    unsigned char m[MLEN + kucp_aimer256f_BYTES];
+    unsigned char m2[MLEN + kucp_aimer256f_BYTES];
+    unsigned char sm[MLEN + kucp_aimer256f_BYTES];
+    unsigned char pk[kucp_aimer256f_PUBLICKEYBYTES];
+    unsigned char sk[kucp_aimer256f_SECRETKEYBYTES];
+
+    unsigned long long kcycles = 0;
+    unsigned long long ecycles = 0;
+    unsigned long long dcycles = 0;
+	unsigned long long cycles1, cycles2;
+
+    unsigned long long mlen, smlen;
+
+	printf("========== KEYGEN SIGN VERIFY SPEED TEST =========\n");
+
+    for(int i = 0; i < TEST_LOOP; i++)
+    {
+        randombytes(m, MLEN);
+
+        cycles1 = cpucycles();
+        kucp_aimer256f_aarch64_crypto_sign_keypair(pk, sk);
+		cycles2 = cpucycles();
+		kcycles += cycles2-cycles1 - cyclegap;
+    }
+    printf("  KEYGEN runs in ................. %8lld cycles\n", kcycles/TEST_LOOP);
+	
+    for(int i = 0; i < TEST_LOOP; i++)
+    {
+        randombytes(m, MLEN);
+
+        cycles1 = cpucycles();
+        kucp_aimer256f_aarch64_crypto_sign(sm, &smlen, m, MLEN, sk);
+		cycles2 = cpucycles();
+		ecycles += cycles2-cycles1 - cyclegap;
+
+        cycles1 = cpucycles();
+        kucp_aimer256f_aarch64_crypto_sign_open(m2, &mlen, sm, smlen, pk);
+		cycles2 = cpucycles();
+		dcycles += cycles2-cycles1 - cyclegap;
+
+    }
+    printf("  SIGN   runs in ................. %8lld cycles\n", ecycles/TEST_LOOP);
+    printf("  VERIFY runs in ................. %8lld cycles\n", dcycles/TEST_LOOP);
+}
+
+static void TEST_SIGN_CLOCK_AIMER256S(void)
+{
+    unsigned char m[MLEN + kucp_aimer256s_BYTES];
+    unsigned char m2[MLEN + kucp_aimer256s_BYTES];
+    unsigned char sm[MLEN + kucp_aimer256s_BYTES];
+    unsigned char pk[kucp_aimer256s_PUBLICKEYBYTES];
+    unsigned char sk[kucp_aimer256s_SECRETKEYBYTES];
+
+    unsigned long long kcycles = 0;
+    unsigned long long ecycles = 0;
+    unsigned long long dcycles = 0;
+	unsigned long long cycles1, cycles2;
+
+    unsigned long long mlen, smlen;
+
+	printf("========== KEYGEN SIGN VERIFY SPEED TEST =========\n");
+
+    for(int i = 0; i < TEST_LOOP; i++)
+    {
+        randombytes(m, MLEN);
+
+        cycles1 = cpucycles();
+        kucp_aimer256s_aarch64_crypto_sign_keypair(pk, sk);
+		cycles2 = cpucycles();
+		kcycles += cycles2-cycles1 - cyclegap;
+    }
+    printf("  KEYGEN runs in ................. %8lld cycles\n", kcycles/TEST_LOOP);
+	
+    for(int i = 0; i < TEST_LOOP; i++)
+    {
+        randombytes(m, MLEN);
+
+        cycles1 = cpucycles();
+        kucp_aimer256s_aarch64_crypto_sign(sm, &smlen, m, MLEN, sk);
+		cycles2 = cpucycles();
+		ecycles += cycles2-cycles1 - cyclegap;
+
+        cycles1 = cpucycles();
+        kucp_aimer256s_aarch64_crypto_sign_open(m2, &mlen, sm, smlen, pk);
 		cycles2 = cpucycles();
 		dcycles += cycles2-cycles1 - cyclegap;
 
@@ -95,18 +494,47 @@ static void TEST_SIGN_CLOCK(void)
 
 int main(void)
 {
-    printf("CRYPTO_ALGNAME        = %s\n", CRYPTO_ALGNAME);
-    printf("CRYPTO_PUBLICKEYBYTES = %d\n", CRYPTO_PUBLICKEYBYTES);
-    printf("CRYPTO_SECRETKEYBYTES = %d\n", CRYPTO_SECRETKEYBYTES);
-    printf("CRYPTO_BYTES          = %d\n", CRYPTO_BYTES);
-
     setup_rdtsc();
-    TEST_SIGN();
-    int i = 0;
-    while(i<5){
-    TEST_SIGN_CLOCK();
-    i++;
-    }
+
+    TEST_SIGN_AIMER128F();
+    TEST_SIGN_AIMER128S();
+    TEST_SIGN_AIMER192F();
+    TEST_SIGN_AIMER192S();
+    TEST_SIGN_AIMER256F();
+    TEST_SIGN_AIMER256S();
+
+    TEST_SIGN_CLOCK_AIMER128F();
+    TEST_SIGN_CLOCK_AIMER128S();
+    TEST_SIGN_CLOCK_AIMER192F();
+    TEST_SIGN_CLOCK_AIMER192S();
+    TEST_SIGN_CLOCK_AIMER256F();
+    TEST_SIGN_CLOCK_AIMER256S();
+
+    // setup_rdtsc();
+    // TEST_SIGN();
+    // int i = 0;
+    // while(i<5){
+    // TEST_SIGN_CLOCK();
+    // i++;
+    // }
 
     return 0;
 }
+
+// int main(void)
+// {
+//     printf("CRYPTO_ALGNAME        = %s\n", CRYPTO_ALGNAME);
+//     printf("CRYPTO_PUBLICKEYBYTES = %d\n", CRYPTO_PUBLICKEYBYTES);
+//     printf("CRYPTO_SECRETKEYBYTES = %d\n", CRYPTO_SECRETKEYBYTES);
+//     printf("CRYPTO_BYTES          = %d\n", CRYPTO_BYTES);
+
+//     setup_rdtsc();
+//     TEST_SIGN();
+//     int i = 0;
+//     while(i<5){
+//     TEST_SIGN_CLOCK();
+//     i++;
+//     }
+
+//     return 0;
+// }
